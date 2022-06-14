a93f6d374b7b9ac07e6af35fcd15340b52b7c4ff
Richard <ricky1001@hotmail.co.uk>
2022-06-14 18:04:07 +0100

    Update newformattemptmine.js

--- public/js/newformattemptmine.js
+++ public/js/newformattemptmine.js
@@ -8,17 +8,20 @@ var currentTab = 0;
 showTab(currentTab)
 
 submit.addEventListener("click", function(e){
-   console.log(tabs)
-   console.log(currentTab)
-   console.log(tabs[2])
+
    e.preventDefault();
-   inputs = tabs[currentTab].getElementsByTagName("input");
+   inputs = tabs[currentTab-1].getElementsByTagName("textarea");
    var valid = true;
    valid = validateForm(inputs, valid);
+   console.log("returned value of valid below")
    console.log(valid)
    if(valid){
+      form.submit();
       form.style.display = "none";
       document.getElementById("thankyou").style.display = "block"
+   }else{
+      console.log("not valid")
+      tabs[currentTab].className = " invalid";
    }
    
 })
@@ -26,44 +29,26 @@ submit.addEventListener("click", function(e){
 
 
 function nextPrev(n){
-   console.log("this is n below top")
-   console.log(n)
+
    var tabs = document.getElementsByClassName("tab");
    var nextBtn = document.getElementById("nextBtn");
    var valid = true;
 
    // Validate the current tab if n = 1
-   console.log("this is n below below")
-   console.log(n)
+
    if(n == 1){
       inputs = tabs[currentTab].getElementsByTagName("input");
       valid = validateForm(inputs, valid);
-      console.log("returned value of valid below")
-      console.log(valid)
 
-      console.log("tabs -1 below")
-      console.log(tabs.length -1)
-      console.log("currenttab below")
-      console.log(currentTab)
-
-
-
-      // if(currentTab == (tabs.length -1) && valid == true){
-      //    console.log("form gets submitted")
-      //    form.submit();
-         
-  
-      // }
    }
 
    if(valid == true){
-      console.log("running n")
+   
       tabs[currentTab].style.display = "none";
 
       currentTab = currentTab + n;
   
       tabs[currentTab].style.display = "block";
-        console.log(currentTab)
       showTab(currentTab)
    }
    
@@ -71,20 +56,18 @@ function nextPrev(n){
 
 //Function to validate a form
 function validateForm(inputsHTMLList, v){
-   console.log("validate form running")
-   console.log(inputsHTMLList)
+
    //loop through the inputs and set invalid class to any applicable fields if any field changes the value of valid to false it will never change back 
    for(var i = 0; i < inputsHTMLList.length; i++){
-      console.log("value of the input below")
-         console.log(inputsHTMLList[i].value)
+
       if(inputsHTMLList[i].value == ""){
-         console.log("setting invalid")
+        
          inputsHTMLList[i].className += " invalid";
          
          v =  false;
       }
       if(inputsHTMLList[i].type == "email"){
-         console.log("checking email")
+        
          var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
          if (!inputsHTMLList[i].value.match(validRegex)) {
             inputsHTMLList[i].className += " invalid"
@@ -92,7 +75,7 @@ function validateForm(inputsHTMLList, v){
           } 
       }
    }
-   console.log("about to return true")
+
    return v;
 }
 
@@ -106,18 +89,21 @@ function showTab(ct){
    tabs[ct].style.display = "block";
 
    // if the tab is o only display the next button
+   console.log("current Tab Below")
+   console.log(currentTab)
    if(ct == 0){
       prevBtn.style.display = "none";
       submitBtn.style.display = "none";
+      nextBtn.style.display = "inline"
    }
    // if the tab is the length of the tab list -1 the next button becomes the submit button
    if(ct == (tabs.length -1)){
       nextBtn.style.display = "none";
       submitBtn.style.display = "inline"
       
-   }else if(ct > 0 && ct <= (tabs.length - 1)){
-      prevBtn.style.display = "block";
+   }else{
       prevBtn.style.display = "inline";
+      nextBtn.style.display = "inline";
       submitBtn.style.display = "none";
    }
 }
