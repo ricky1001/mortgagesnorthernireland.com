15ef46b41d1df1f5d08ca4fa4c9ecb4a1b4ea6ec
Richard <ricky1001@hotmail.co.uk>
2022-07-15 16:13:58 +0100

    Update testing-fetch.js

--- routes/testing-fetch.js
+++ routes/testing-fetch.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.post("/testing-fetch", function(req, res){
      console.log("here r there")
-     console.log(req) 
+     console.log(req.body) 
      return res.status(200).json({
           status: 200,
           message: "whatthe  fuck"
