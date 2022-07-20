75c9b58a81b8c13d47b99aa26352e214dae68d4f
Richard <ricky1001@hotmail.co.uk>
2022-07-20 09:24:38 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,16 +1,16 @@
 
 
 
-exports.postingData = (req, res, next) => {
+// exports.postingData = (req, res, next) => {
 
-    console.log("req body below")
-    console.log(req.body)
-    console.log("req body above")
-    return res.status(200).json({
-         status: 200,
-         message: "whatthe  fuck"
-    })
-}
+//     console.log("req body below")
+//     console.log(req.body)
+//     console.log("req body above")
+//     return res.status(200).json({
+//          status: 200,
+//          message: "whatthe  fuck"
+//     })
+// }
 
 
 
